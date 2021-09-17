9a70fc040e7f3ecea1016c137d11934ed3509411
Abby Henrikson <apeskind999@gmail.com>
2021-09-17 16:03:16 -0500

    Lab 2 - Add Meta Data

--- public/index.php
+++ public/index.php
@@ -12,9 +12,9 @@ $content="<h1>ahenrikson0582 Users</h1>";
 $stmt = $pdo->query('SELECT * FROM users');
 
 while($row = $stmt->fetch()){
-  $content .= "<div><a href=\"view.php?id={$row['id']}\">{$row['first_name']} {$row['last_name']}</a></div>";
+  $content .= "<div><a href=\"../public/users/view.php?id={$row['id']}\">{$row['first_name']} {$row['last_name']}</a></div>";
 }
 
-$content .= "<br><hr><div><a href=\"add.php\">New User</a></div><br>";
+$content .= "<br><hr><div><a href=\"../public/users/add.php\">New User</a></div><br>";
 
 require '../core/layout.php';
